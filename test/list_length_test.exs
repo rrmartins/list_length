76cc4a7680a00f785096ec0c925518cea02fc4af
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-08 23:00:43 +0000

    make tests

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -1,8 +1,29 @@
 defmodule ListLengthTest do
   use ExUnit.Case
-  doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "call/1" do
+    test "returns the size" do
+      size = [1,2,4,6,3,"dsadas"]
+             |> ListLength.call
+      expected_size = 6
+
+      assert expected_size == size
+    end
+
+    test "returns the size 0 when send nothing" do
+      size = []
+             |> ListLength.call
+      expected_size = 0
+
+      assert expected_size == size
+    end
+
+    test "returns the size when send chars" do
+      size = 'rodrigo'
+             |> ListLength.call
+      expected_size = 7
+
+      assert expected_size == size
+    end
   end
 end
